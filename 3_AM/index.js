32f75ea67e250b61924201135cb5e0a83c370f6a
Addison Zhang <zhangx1@college.harvard.edu>
2019-01-09 12:56:38 -0500

    shitty website

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
 
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+// app.use(express.static(__dirname + "/View"));
+// app.use(express.static(__dirname + "/Script"));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/first", (req, res) => {
+  res.sendFile(__dirname + '/first.html');
+});
+
+app.get("/movies", (req, res) => {
+  res.sendFile(__dirname + '/movies.html');
+});
+
+app.get("/third", (req, res) => {
+  res.sendFile(__dirname + '/third.html');
+});
+
+app.get("/fourth", (req, res) => {
+  res.sendFile(__dirname + '/fourth.html');
+});
+// app.get('/first')
 
 app.listen(3000);
 console.log("Running at port 3000");
